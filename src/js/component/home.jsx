fa1cd77ef838b0cad550e8bb4dbf10582f76096e
jorgepardor <jorgeluis.mail@gmail.com>
2022-02-15 22:25:17 +0000

    añadidas las funcionalidades, no completamente diseñado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import reactDom from "react-dom";
 
 import Task from "./task.jsx";
-import Tasklist from "./tasklist.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<div className="newtask">
-				<Task />
-			</div>
-			<div className="taskList">
-				<Tasklist />
+		<div className="mainbox">
+			<div className="container">
+				<div className="flex-row ">
+					<div className="col-12 d-flex justify-content-center mt-5">
+						<Task />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Tasklist from "./tasklist.jsx";
 
 //create your first component
 const Task = () => {
@@ -7,29 +6,65 @@ const Task = () => {
 	const [text, setText] = useState("");
 
 	const handleChange = (event) => {
+		event.preventDefault();
 		console.log(event.target.value);
 		setText(event.target.value);
+		this.setText({
+			value: "",
+		});
 	};
+
+	const Delete = (item) =>
+		setTask(task.filter((deleteMe) => item != deleteMe));
+	// console.log(index);
 
 	const submit = () => {
 		setTask([...task, text]);
 	};
 
 	return (
-		<div>
-			{/* <div>
-				<select onChange={handleChange}>
-					<option>Priority</option>
-					<option value="red">Urgent</option>
-					<option value="yellow">Low Stock</option>
-					<option value="green">Not really needed</option>
-				</select>
-			</div> */}
+		<div className="listbox border rounded-2 col-8">
+			<div className="title my-3 px-3">
+				<h1 className="display-2 text-center">
+					<span className="fst-italic">Mise en place</span> list 🧑‍🍳
+				</h1>
+			</div>
 
-			<input type="text" name="description" onChange={handleChange} />
-			<button onClick={submit}></button>
+			<div class="input-group mb-3 d-flex justify-content-between px-3">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="List your ingredients here"
+					aria-label="List your ingredients here"
+					aria-describedby="button-addon1"
+					onChange={handleChange}
+				/>
+				<button
+					className="btn btn-outline-success btn-sm"
+					id="button-addon2"
+					onClick={submit}>
+					Add item ✔️
+				</button>
+			</div>
+
+			<div className="entryList">
+				{task.map((value, index) => (
+					<ul className="list-group px-2">
+						<li
+							className="list-group-item d-flex justify-content-between mb-2"
+							key={index}>
+							<span>{value}</span>
+
+							<button
+								className="btn btn-outline-danger btn-sm"
+								onClick={() => Delete(value)}>
+								❌
+							</button>
+						</li>
+					</ul>
+				))}
+			</div>
 		</div>
 	);
 };
-
 export default Task;
